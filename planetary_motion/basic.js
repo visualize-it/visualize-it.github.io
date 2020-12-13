0d219662f1690a0f0bbf15873d21f0cbbe9e10c4
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2020-12-13 19:34:20 +0530

    First working simulation

--- planetary_motion/basic.js
+++ planetary_motion/basic.js
@@ -7,6 +7,9 @@ let context = canvas.getContext("2d");
 let car = document.getElementById("car");
 let remove_button = document.getElementById("remove-button");
 
+let pause_button = document.getElementById("pause-button");
+let resume_button = document.getElementById("resume-button");
+
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     mobile = true;
 } else {
@@ -36,6 +39,8 @@ let animate = window.requestAnimationFrame
 
 window.onload = function() {
     initialParams();
+    substituteParams();
+    startSimulation();
     animate(step);
 }
 
@@ -50,26 +55,32 @@ function newBody() {
     car.innerHTML += `
     <div class="col s6">
     <p id="body-${num_entities}" class="center-align">Planet ${num_entities - 1}</p>
-    <input id="mass-${num_entities}" placeholder="Mass" type="number">
-    <input id="x-${num_entities}" placeholder="X-coordinate" type="number">
-    <input id="y-${num_entities}" placeholder="Y-coordinate" type="number">
-    <input id="vx-${num_entities}" placeholder="Horizontal velocity" type="number">
-    <input id="vy-${num_entities}" placeholder="Vertical velocity" type="number">
+    <input id="m-${num_entities}" placeholder="Mass" type="number" onchange="updateParams()">
+    <input id="x-${num_entities}" placeholder="X-coordinate" type="number" onchange="updateParams()">
+    <input id="y-${num_entities}" placeholder="Y-coordinate" type="number" onchange="updateParams()">
+    <input id="vx-${num_entities}" placeholder="Horizontal velocity" type="number" onchange="updateParams()">
+    <input id="vy-${num_entities}" placeholder="Vertical velocity" type="number" onchange="updateParams()">
 </div>
     `;
 
     if(num_entities > 2) {
         remove_button.style.display = "block";
     }
+    substituteParams();
 }
 
 function removeBody() {
     if (num_entities > 2) {
+
         num_entities -= 1;
+        while(bodies.length > num_entities) {
+            bodies.pop();
+        }
+
         car.innerHTML = `
         <div class="col s6">
         <p id="body-1" class="center-align">Star</p>
-        <input id="mass-1" placeholder="Mass" type="number">
+        <input id="m-1" placeholder="Mass" type="number">
         <input id="x-1" placeholder="X-coordinate" type="number">
         <input id="y-1"  placeholder="Y-coordinate" type="number">
         <input id="vx-1" placeholder="Horizontal velocity" type="number">
@@ -80,11 +91,11 @@ function removeBody() {
             car.innerHTML += `
             <div class="col s6">
             <p id="body-${entity}" class="center-align">Planet ${entity - 1}</p>
-            <input id="mass-${entity}" placeholder="Mass" type="number">
-            <input id="x-${entity}" placeholder="X-coordinate" type="number">
-            <input id="y-${entity}" placeholder="Y-coordinate" type="number">
-            <input id="vx-${entity}" placeholder="Horizontal velocity" type="number">
-            <input id="vy-${entity}" placeholder="Vertical velocity" type="number">
+            <input id="m-${entity}" placeholder="Mass" type="number" onchange="updateParams()">
+            <input id="x-${entity}" placeholder="X-coordinate" type="number" onchange="updateParams()">
+            <input id="y-${entity}" placeholder="Y-coordinate" type="number" onchange="updateParams()">
+            <input id="vx-${entity}" placeholder="Horizontal velocity" type="number" onchange="updateParams()">
+            <input id="vy-${entity}" placeholder="Vertical velocity" type="number" onchange="updateParams()">
         </div>
             `
         }
@@ -92,6 +103,90 @@ function removeBody() {
 
     if(num_entities == 2) {
         remove_button.style.display = "none";
+    }
+    substituteParams();
+}
+
+function initialParams() {
+    paused = false;
+    num_entities = init_num_entities;
+
+    bodies.push(
+        {
+            m: init_star_mass,
+            x: init_star_x,
+            y: init_star_y,
+            vx: init_star_vx,
+            vy: init_star_vy,
+        }
+    )
+    bodies.push(
+        {
+            m: init_planet_mass,
+            x: init_planet_x,
+            y: init_planet_y,
+            vx: init_planet_vx,
+            vy: init_planet_vy,
+        }
+    )
+
+    if (num_entities > 2) {
+        remove_button.style.display = "block";
+    }
+    else {
+        remove_button.style.display = "none";
+    }
+}
+
+function substituteParams() {
+    for (let entity = 1; entity <= num_entities; entity++) {
+        if (bodies[entity - 1] !== undefined) {
+            m_element = document.getElementById(`m-${entity}`);
+            m_element.value = `${bodies[entity - 1].m}`;
+
+            x_element = document.getElementById(`x-${entity}`);
+            x_element.value = `${bodies[entity - 1].x}`;
+
+            y_element = document.getElementById(`y-${entity}`);
+            y_element.value = `${bodies[entity - 1].y}`;
+
+            vx_element = document.getElementById(`vx-${entity}`);
+            vx_element.value = `${bodies[entity - 1].vx}`;
+
+            vy_element = document.getElementById(`vy-${entity}`);
+            vy_element.value = `${bodies[entity - 1].vy}`;
+        }
+    }
+}
+
+function updateParams() {
+    bodies = [];
+
+    for (let entity = 1; entity <= num_entities; entity++) {
+        m_element = document.getElementById(`m-${entity}`);
+        m = parseFloat(m_element.value);
+
+        x_element = document.getElementById(`x-${entity}`);
+        x = parseFloat(x_element.value);
+
+        y_element = document.getElementById(`y-${entity}`);
+        y = parseFloat(y_element.value);
+
+        vx_element = document.getElementById(`vx-${entity}`);
+        vx = parseFloat(vx_element.value);
+
+        vy_element = document.getElementById(`vy-${entity}`);
+        vy = parseFloat(vy_element.value);
+
+        bodies.push(
+            {
+                m: m,
+                x: x,
+                y: y,
+                vx: vx,
+                vy: vy,
+            }
+        )
     }
 }
 
--- planetary_motion/planetary_motion.js
+++ planetary_motion/planetary_motion.js
@@ -1,38 +1,121 @@
-// Initial Params
+// Initial params
 let init_num_entities = 2;
 let init_star_mass = 1000;
 let init_star_x = 0, init_star_y = 0;
 let init_star_vx = 0, init_star_vy = 0;
 
 let init_planet_mass = 10;
-let init_planet_x = 50, init_planet_y = 0;
+let init_planet_x = 5, init_planet_y = 0;
 let init_planet_vx = 0, init_planet_vy = 10;
 
-// Transitive params
-let num_entities
 let bodies = [];
+
+// Simulation params
+let num_entities
 let scale, max_distance;
+let params = [];
+let paused = false;
+let G = 1, prec = 0.01;
+let radius = 5;
+
+// Transitive elements
+let m_element, x_element, y_element, vx_element, vy_element;
+let ax, ay;
+let m, x, y, vx, vy;
+
+function calcScale() {
+    let heaviest_body = bodies[getHeaviestBody()];
+    let lightest_body = bodies[getLightestBody()];
+
+    let initial_energy = (- G * heaviest_body.m * lightest_body.m / distance(heaviest_body.x, heaviest_body.y, lightest_body.x, lightest_body.y));
+    initial_energy += 0.5 * lightest_body.m * (Math.pow(lightest_body.vx, 2) + Math.pow(lightest_body.vy, 2));
+    let maximum_displacement = (- G * heaviest_body.m * lightest_body.m / initial_energy);
+    scale = canvas_width / (2 * maximum_displacement);
+}
 
 function update() {
+    for (let i = 0; i < params.length; i++) {
+        ax = 0;
+        ay = 0;
 
+        for (let j = 0; j < params.length; j++) {
+            if (i != j) {
+                ax += (- G * params[j].m * (params[i].x - params[j].x) / Math.pow(distance(params[i].x, params[i].y, params[j].x, params[j].y), 3));
+                ay += (- G * params[j].m * (params[i].y - params[j].y) / Math.pow(distance(params[i].x, params[i].y, params[j].x, params[j].y), 3));
+            }
+        }
+
+        params[i].vx += (ax * prec);
+        params[i].vy += (ay * prec);
+
+        params[i].x += (params[i].vx * prec);
+        params[i].y += (params[i].vy * prec);
+    }
 }
 
 function render() {
     context.fillStyle = "#000000";
     context.fillRect(0, 0, canvas_width, canvas_height);
+
+    context.strokeStyle = "#ffffff";
+    for (let body of params) {
+        context.beginPath();
+        context.arc(transformX(body.x * scale) - radius, transformY(body.y * scale) - radius, radius, 0, 2 * Math.PI);
+        context.stroke();
+    }
 }
 
-function initialParams() {
-    num_entities = init_num_entities;
-    if(num_entities > 2) {
-        remove_button.style.display = "block";
+function startSimulation() {
+    console.log(bodies);
+
+    updateParams();
+    calcScale();
+    params = [];
+    for (let body of bodies) {
+        params.push(body);
+    }
+}
+
+function pause() {
+    if (paused) {
+        paused = false;
+        pause_button.innerHTML = "Pause";
     }
     else {
-        remove_button.style.display = "none";
+        paused = true;
+        pause_button.innerHTML = "Resume";
     }
 }
 
-function updateParams() {
+function restart() {
 
 }
+
+function getLightestBody() {
+    let index = 0;
+    let lowest_mass = Infinity;
+
+    for (let i = 0; i < bodies.length; i++) {
+        if (bodies[i].m < lowest_mass) {
+            lowest_mass = bodies[i].m;
+            index = i;
+        }
+    }
+    return index;
+}
+
+function getHeaviestBody() {
+    let index = 0;
+    let highest_mass = -Infinity;
+
+    for (let i = 0; i < bodies.length; i++) {
+        if (bodies[i].m > highest_mass) {
+            highest_mass = bodies[i].m;
+            index = i;
+        }
+    }
+    return index;
+}
+
+
 

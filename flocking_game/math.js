0f51db8bc585422c5e8eedd479dc93b5177eb023
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2021-07-10 12:48:33 +0530

    Fixed minor bugs

--- flocking_game/math.js
+++ flocking_game/math.js
@@ -61,18 +61,20 @@ function minimiseDifference(orient_angle, boid_angle) {
 
 function gaussianNoise(min, max, skew = 1) {
     let u = 0, v = 0;
-    while(u === 0) u = Math.random() //Converting [0,1) to (0,1)
-    while(v === 0) v = Math.random()
-    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
-    
-    num = num / 10.0 + 0.5 // Translate to 0 -> 1
-    if (num > 1 || num < 0) 
-      num = randn_bm(min, max, skew) // resample between 0 and 1 if out of range
-    
-    else{
-      num = Math.pow(num, skew) // Skew
-      num *= max - min // Stretch to fill range
-      num += min // offset to min
+    while (u === 0) u = Math.random();
+    while (v === 0) v = Math.random();
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+
+    num = num / 10.0 + 0.5;
+    // num is now a Gaussian between 0 and 1
+    if (num > 1 || num < 0) {
+        // resample
+        num = gaussianNoise(min, max, skew);
     }
-    return num
-  }
+    else {
+        num = Math.pow(num, skew);
+        num *= max - min;
+        num += min;
+    }
+    return num;
+}

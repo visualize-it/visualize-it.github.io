d0cbaf65b2d5af6b90b9e5984a39d34bd79f205a
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2021-07-23 21:46:28 +0530

    Prime counting initial and final commit

---
+++ prime_counting/basic.js
@@ -0,0 +1,52 @@
+let screen_width = window.innerWidth, screen_height = window.innerHeight;
+let fps = 24;
+
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+let limit_input = document.getElementById("limit-input");
+let report_display = document.getElementById("report-display");
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    mobile = true;
+} else {
+    mobile = false;
+}
+
+if (mobile) {
+    canvas_width = 0.9 * screen_width;
+}
+else {
+    canvas_width = 0.5 * screen_width;
+}
+canvas_height = canvas_width / 1.62;
+
+canvas.width = canvas_width;
+canvas.height = canvas_height;
+
+let animate = window.requestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || function (callback) {
+        window.setTimeout(callback, 1000 / fps);
+    };
+
+window.onload = function () {
+    initParams();
+    animate(step);
+}
+
+function step() {
+    update();
+    render();
+    animate(step);
+}
+
+function graph() {
+    limit = parseFloat(limit_input.value);
+    initialise();
+}
+
+function report() {
+    report_display.textContent = `Number of primes until ${current_num}: ${frequencies[frequencies.length - 1]}`;
+}

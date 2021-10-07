b0e98e141ded05e5f34a7b3f7c2a751829812427
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2021-10-07 19:55:44 +0530

    Added layouts to natural selection

--- natural_selection/natural_selection.js
+++ natural_selection/natural_selection.js
@@ -28,7 +28,7 @@ function makePool() {
     let new_avg_fitness = 0;
     let max_fitness = 0;
     num_reached = 0;
-    
+
     for (let boid of boids) {
         new_avg_fitness += boid.fitness;
         if (boid.fitness > max_fitness) {
@@ -54,12 +54,12 @@ function makePool() {
 
     fitness_display.innerHTML = `Maximum fitness: ${max_fitness.toFixed(6)} <br>`;
     fitness_display.innerHTML += `Average fitness: ${new_avg_fitness.toFixed(6)} <br>`;
-    if(generation > 2) {
+    if (generation > 2) {
         let percent_change = 100 * (new_avg_fitness - avg_fitness) / avg_fitness;
         fitness_display.innerHTML += `Evolution: ${percent_change.toFixed(2)} % <br>`;
     }
     fitness_display.innerHTML += `${num_reached} boids reached target <br>`;
-    if(num_reached > 0) {
+    if (num_reached > 0) {
         fitness_display.innerHTML += `Fastest time in prev gen: ${fastest_time} steps`;
     }
 
@@ -78,10 +78,10 @@ function mate() {
         parent1_dna = randomParent().gene.dna;
         parent2_dna = randomParent().gene.dna;
 
-        for(let i = 0; i < lifespan / 2; i++) {
+        for (let i = 0; i < lifespan / 2; i++) {
             new_dna.push(parent1_dna[i]);
         }
-        for(let i = Math.floor(lifespan / 2); i < lifespan; i++) {
+        for (let i = Math.floor(lifespan / 2); i < lifespan; i++) {
             new_dna.push(parent2_dna[i]);
         }
         new_gene = new Gene(new_dna);
@@ -103,12 +103,12 @@ function mate() {
 function update() {
     let num_alive = 0;
 
-    for(let boid of boids) {
-        if(boid.alive && !boid.reached) {
+    for (let boid of boids) {
+        if (boid.alive && !boid.reached) {
             num_alive++;
         }
     }
-    if(num_alive == 0) {
+    if (num_alive == 0) {
         current_age = lifespan;
     }
 
@@ -168,18 +168,17 @@ function initParams() {
     wall_width_factor = 5;
     wall_height_factor = 30;
 
+    source = new Source(canvas_width / 2, 7 * canvas_height / 8);
+    target = new Target(canvas_width / 2, canvas_height / 8);
+
+    walls.push(new Wall(canvas_width / 2, 2 * canvas_height / 3, "h"));
+    walls.push(new Wall(3 * canvas_width / 10, canvas_height / 3, "h"));
+    walls.push(new Wall(7 * canvas_width / 10, canvas_height / 3, "h"));
+
     initialize();
 }
 
 function initialize() {
-    if(walls.length == 0) {
-        walls.push(new Wall(2 * canvas_width / 5, 2 * canvas_height / 5, "h"));
-        walls.push(new Wall(3 * canvas_width / 5, 2 * canvas_height / 5, "h"));
-    }
-    
-    source = new Source(canvas_width / 2, 7 * canvas_height / 8);
-    target = new Target(canvas_width / 2, canvas_height / 8);
-
     updateMovables();
     makeInitPopulation();
 
@@ -189,7 +188,7 @@ function initialize() {
     gene_context.fillStyle = "#000000";
     gene_context.fillRect(0, 0, gene_canvas.width, gene_canvas.height);
 
-    if(paused) {
+    if (paused) {
         pauseToggle();
     }
 }
@@ -207,6 +206,29 @@ function makeInitPopulation() {
     generation_display.innerHTML = `Generation: ${generation}`;
 }
 
+function layouts(name) {
+    walls = [];
+    if (name == "default") {
+        walls.push(new Wall(canvas_width / 10, 2 * canvas_height / 3, "h"));
+        walls.push(new Wall(3 * canvas_width / 10, 2 * canvas_height / 3, "h"));
+        walls.push(new Wall(5 * canvas_width / 10, 2 * canvas_height / 3, "h"));
+        walls.push(new Wall(7 * canvas_width / 10, 1 * canvas_height / 3, "h"));
+        walls.push(new Wall(9 * canvas_width / 10, 1 * canvas_height / 3, "h"));
+        walls.push(new Wall(5 * canvas_width / 10, 1 * canvas_height / 3, "h"));
+    }
+    else if (name == "simple") {
+        walls.push(new Wall(2 * canvas_width / 5, 2 * canvas_height / 5, "h"));
+        walls.push(new Wall(3 * canvas_width / 5, 2 * canvas_height / 5, "h"));
+    }
+    else if (name == "detour") {
+        walls.push(new Wall(canvas_width / 2, 2 * canvas_height / 3, "h"));
+        walls.push(new Wall(3 * canvas_width / 10, canvas_height / 3, "h"));
+        walls.push(new Wall(7 * canvas_width / 10, canvas_height / 3, "h"));
+    }
+    window.scrollTo(0, 350);
+    initialize();
+}
+
 function drawLifespan() {
     let width = Math.floor(canvas_width * (1 - current_age / lifespan));
     context.fillStyle = "#ffffff";
@@ -219,11 +241,11 @@ function drawGene() {
 
     let width = Math.ceil(canvas_width / lifespan);
     let hue;
-    for(let i = 1; i < lifespan; i += width) {
+    for (let i = 1; i < lifespan; i += width) {
         hue = Math.floor(255 * (max_fitness_gene.dna[i] + half_angle) / (2 * half_angle));
         gene_context.fillStyle = `hsl(${hue}, 50%, 50%)`;
         gene_context.fillRect(i * width, 0, (i + 1) * width, gene_canvas.height);
-    } 
+    }
 }
 
 function updateMovables() {
@@ -254,7 +276,7 @@ function randomChild() {
 }
 
 function pauseToggle() {
-    if(paused) {
+    if (paused) {
         paused = false;
         pause_button.innerHTML = "Pause";
     }

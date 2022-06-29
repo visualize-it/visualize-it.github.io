0d5c08ca3726dab20b07cb94302df2814da6be55
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2022-06-29 20:46:01 +0530

    Initialized firefly synchronization

---
+++ firefly_synchronization/simulation.js
@@ -0,0 +1,16 @@
+function update() {
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, canvas_width, canvas_height);
+}
+
+function render() {
+
+}
+
+function updateParams(variable) {
+
+}
+
+function initParams() {
+
+}

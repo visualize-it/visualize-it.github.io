7841798cffa33736ca651c1706714f422f487ec9
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2022-08-31 23:20:21 +0530

    Initialized artificial life

---
+++ artificial_life/simulation.js
@@ -0,0 +1,16 @@
+function update() {
+
+}
+
+function render() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas_width, canvas_height);
+}
+
+function updateParams(variable) {
+
+}
+
+function initParams() {
+
+}

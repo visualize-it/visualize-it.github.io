1ce21e186e8a5b30fcb6f39c2c68fe95f81cc461
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2024-06-01 17:28:53 +0530

    Init sandpile model

---
+++ sandpile_model/simulation.js
@@ -0,0 +1,16 @@
+function update() {
+
+}
+
+function render() {
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function updateParams(variable) {
+
+}
+
+function initParams() {
+
+}
